04a2d735abec39cd1765f897fa004611f8e12b19
Apurvanaruka <apurvanaruka1@gmail.com>
2023-12-16 08:37:26 +0530

    project setup

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+    return (
+        <h1 className="text-4xl">hello</h1>
+    )
+}
+
+root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);
