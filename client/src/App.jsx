01a8f96b82d0b0964b82ef1b9dad9876b74a3c4c
Sanjana Gandhi <sanjanagandhi1810@gmail.com>
2023-12-20 15:47:33 +0530

    uodate app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Form1 from './components/Form1';
+import Login from './components/Login';
 import Header from './components/Header';
 import About from './components/About';
-import Contact from './components/contact';
+import contact from './components/contact';
 import Profile from './components/Profile';
 
 const App = () => {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        // Implement your login logic here
+        // For simplicity, just toggling the login state
+        setLoggedIn(!isLoggedIn);
+    };
+
     return (
-        <Router>
-            <div>
-                <Header />
-                <hr />
-                <Switch>
-                    <Route path="/form1" component={Form1} />
-                    <Route path="/about" component={About} />
-                    <Route path="/contact" component={Contact} />
-                    <Route path="/profile" component={Profile} />
-                </Switch>
-            </div>
-        </Router>
+<Header/>        
     );
 };
 
